646c2d565425b9e17b46d3051c09dad197a9d099
Retr0327 <retr0327.dev@gmail.com>
2023-12-21 19:42:22 +0800

    Integrate router, CORS, and logger middleware

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,9 @@
 import Koa from 'koa';
 import koaEtag from 'koa-etag';
+import router from '@routes';
+import cors from '@middlewares/cors';
 import bodyParser from 'koa-bodyparser';
+import logger from '@middlewares/logger';
 import koaConditionalGet from 'koa-conditional-get';
 
 const app = new Koa();
@@ -8,5 +11,10 @@ const app = new Koa();
 app.use(koaConditionalGet());
 app.use(koaEtag());
 app.use(bodyParser());
+app.use(cors());
+app.use(logger());
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 export default app;
