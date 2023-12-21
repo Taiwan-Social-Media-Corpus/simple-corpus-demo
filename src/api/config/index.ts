96efd2f3646363fe95478fe77e0a88be08958362
Retr0327 <retr0327.dev@gmail.com>
2023-12-21 21:00:31 +0800

    Implement environment configuration with Zod

---
+++ src/api/config/index.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+// eslint-disable-next-line import/newline-after-import
+require('dotenv').config();
+
+const configSchema = z
+  .object({
+    // redis
+    REDIS_URL: z.string().min(1),
+    // blacklab
+    BLACKLAB_HOST: z.string().min(1),
+  })
+  .transform((env) => ({
+    redisURL: env.REDIS_URL,
+    blackURL: `http://${env.BLACKLAB_HOST}:8080/blacklab-server`,
+  }));
+
+export const config = configSchema.parse(process.env);
+export default () => config;
